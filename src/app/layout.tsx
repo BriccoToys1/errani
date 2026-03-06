a94193bae2e465b5eb2f8ccfa98df105a1c1e8a6
Martin <martin@Martins-MacBook-Air.local>
2026-03-06 12:34:47 +0300

    perf: convert fonts to WOFF2, remove Google Fonts, add preload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,11 +70,12 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          href="/fonts/ZalandoSans.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
         />
         <script
           type="application/ld+json"
